cda9610aed14e6852b967c7becf93fa381ed8a94
yudi <yudi@shanshu.ai>
2020-03-10 22:15:49 +0800

    add VAE

---
+++ daisy/model/SLiMRecommender.py
@@ -0,0 +1,111 @@
+import sys
+import time
+import numpy as np
+import pandas as pd
+import scipy.sparse as sp
+from sklearn.linear_model import ElasticNet
+
+# TODO this recommender must change to multiprocessing mode and compress into a more beautiful way
+
+class SLIM(object):
+    def __init__(self, user_num, item_num, topk=100,
+                 l1_ratio=0.1, alpha=1.0, positive_only=True):
+        self.md = ElasticNet(alpha=alpha, 
+                             l1_ratio=l1_ratio, 
+                             positive=positive_only, 
+                             fit_intercept=False,
+                             copy_X=False,
+                             precompute=True,
+                             selection='random',
+                             max_iter=100,
+                             tol=1e-4)
+
+        self.item_num = item_num
+        self.user_num = user_num
+        self.topk = topk
+
+        print(f'user num: {user_num}, item num: {item_num}')
+
+    def fit(self, df, verbose=True):
+        train = self._convert_df(self.user_num, self.item_num, df)
+
+        dataBlock = 10000000
+
+        rows = np.zeros(dataBlock, dtype=np.int32)
+        cols = np.zeros(dataBlock, dtype=np.int32)
+        values = np.zeros(dataBlock, dtype=np.float32)
+
+        numCells = 0
+
+        start_time = time.time()
+        start_time_printBatch = start_time
+
+        for currentItem in range(self.item_num):
+            y = train[:, currentItem].toarray()
+
+            # set the j-th column of X to zero
+            start_pos = train.indptr[currentItem]
+            end_pos = train.indptr[currentItem + 1]
+
+            current_item_data_backup = train.data[start_pos: end_pos].copy()
+            train.data[start_pos: end_pos] = 0.0
+
+            # fit one ElasticNet model per column
+            self.md.fit(train, y)
+
+            nonzero_model_coef_index = self.md.sparse_coef_.indices
+            nonzero_model_coef_value = self.md.sparse_coef_.data
+
+            local_topK = min(len(nonzero_model_coef_value)-1, self.topk)
+
+            relevant_items_partition = (-nonzero_model_coef_value).argpartition(local_topK)[0:local_topK]
+            relevant_items_partition_sorting = np.argsort(-nonzero_model_coef_value[relevant_items_partition])
+            ranking = relevant_items_partition[relevant_items_partition_sorting]
+
+            for index in range(len(ranking)):
+                if numCells == len(rows):
+                    rows = np.concatenate((rows, np.zeros(dataBlock, dtype=np.int32)))
+                    cols = np.concatenate((cols, np.zeros(dataBlock, dtype=np.int32)))
+                    values = np.concatenate((values, np.zeros(dataBlock, dtype=np.float32)))
+
+                rows[numCells] = nonzero_model_coef_index[ranking[index]]
+                cols[numCells] = currentItem
+                values[numCells] = nonzero_model_coef_value[ranking[index]]
+
+                numCells += 1
+
+            train.data[start_pos:end_pos] = current_item_data_backup
+
+            if verbose and (time.time() - start_time_printBatch > 300 or (currentItem + 1) % 1000 == 0 or currentItem == self.item_num - 1):
+                print('{}: Processed {} ( {:.2f}% ) in {:.2f} minutes. Items per second: {:.0f}'.format(
+                     'SLIMElasticNetRecommender',
+                     currentItem+1,
+                     100.0* float(currentItem+1)/self.item_num,
+                     (time.time()-start_time)/60,
+                     float(currentItem)/(time.time()-start_time)))
+
+                sys.stdout.flush()
+                sys.stderr.flush()
+
+                start_time_printBatch = time.time()
+
+        # generate the sparse weight matrix
+        self.W_sparse = sp.csr_matrix((values[:numCells], (rows[:numCells], cols[:numCells])),
+                                       shape=(self.item_num, self.item_num), dtype=np.float32)
+
+        train = train.tocsr()
+        self.A_tilde = train.dot(self.W_sparse).A
+
+    def predict(self, u, i):
+
+        return self.A_tilde[u, i]
+
+    def _convert_df(self, user_num, item_num, df):
+        '''Process Data to make WRMF available'''
+        ratings = list(df['rating'])
+        rows = list(df['user'])
+        cols = list(df['item'])
+
+        mat = sp.csc_matrix((ratings, (rows, cols)), shape=(user_num, item_num))
+
+        return mat
---
+++ daisy/model/VAERecommender.py
@@ -0,0 +1,186 @@
+import os
+import numpy as np
+from tqdm import tqdm
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+
+class VAE(nn.Module):
+    def __init__(self,
+                 rating_mat,
+                 q_dims=None,
+                 q=0.5,
+                 lr=1e-3,
+                 reg_1=0.,
+                 reg_2=0.,
+                 loss_type='CL',
+                 gpuid='0',
+                 early_stop=True):
+        super(VAE, self).__init__()
+
+        self.lr = lr
+        self.reg_1 = reg_1
+        self.reg_2 = reg_2
+        self.loss_type = loss_type
+        self.early_stop = early_stop
+
+        os.environ['CUDA_VISIBLE_DEVICES'] = gpuid
+        cudnn.benchmark = True
+
+        user_num, item_num = rating_mat.shape
+        self.user_num = user_num
+        self.item_num = item_num
+        self.rating_mat = rating_mat
+
+        p_dims = [200, 600, item_num]
+        self.p_dims = p_dims
+        if q_dims:
+            assert q_dims[0] == p_dims[-1], "In and Out dimensions must equal to each other"
+            assert q_dims[-1] == p_dims[0], "Latent dimension for p- and q- network mismatches."
+            self.q_dims = q_dims
+        else:
+            self.q_dims = p_dims[::-1]
+        
+        # Last dimension of q- network is for mean and variance
+        tmp_q_dims = self.q_dims[:-1] + [self.q_dims[-1] * 2]  # essay setting only focus on 2 encoder
+        self.q_layers = nn.ModuleList(
+            [nn.Linear(d_in, d_out) for d_in, d_out in zip(tmp_q_dims[:-1], tmp_q_dims[1:])]
+        )
+        self.p_layers = nn.ModuleList(
+            [nn.Linear(d_in, d_out) for d_in, d_out in zip(self.p_dims[:-1], self.p_dims[1:])]
+        )
+        self.drop = nn.Dropout(q)
+        self._init_weights()
+
+    def _init_weights(self):
+        for layer in self.q_layers:
+            # Xavier Initialization for weights
+            size = layer.weight.size()
+            fan_out = size[0]
+            fan_in = size[1]
+            std = np.sqrt(2.0/(fan_in + fan_out))
+            layer.weight.data.normal_(0.0, std)
+
+            # Normal Initialization for Biases
+            layer.bias.data.normal_(0.0, 0.001)
+        
+        for layer in self.p_layers:
+            # Xavier Initialization for weights
+            size = layer.weight.size()
+            fan_out = size[0]
+            fan_in = size[1]
+            std = np.sqrt(2.0/(fan_in + fan_out))
+            layer.weight.data.normal_(0.0, std)
+
+            # Normal Initialization for Biases
+            layer.bias.data.normal_(0.0, 0.001)
+    
+    def encode(self, input):
+        h = F.normalize(input)
+        h = self.drop(h)
+        for i, layer in enumerate(self.q_layers):
+            h = layer(h)
+            if i != len(self.q_layers) - 1:
+                h = F.tanh(h)
+            else:
+                mu = h[:, :self.q_dims[-1]]  
+                logvar = h[:, self.q_dims[-1]:]
+        
+        return mu, logvar
+
+    def reparameterize(self, mu, logvar):
+        if self.training:
+            std = torch.exp(0.5 * logvar)
+            eps = torch.randn_like(std)
+            return eps.mul(std).add_(mu)
+        else:
+            return mu
+
+    def decode(self, z):
+        h = z
+        for i, layer in enumerate(self.p_layers):
+            h = layer(h)
+            if i != len(self.p_layers) - 1:
+                h = F.tanh(h)
+        return h
+
+    def forward(self, input):
+        mu, logvar = self.encode(input)
+        z = self.reparameterize(mu, logvar)
+        return self.decode(z), mu, logvar
+
+    def fit(self, train_loader):
+        if torch.cuda.is_available():
+            self.cuda()
+        else:
+            self.cpu()
+
+        if self.loss_type == 'CL':
+            criterion = nn.BCEWithLogitsLoss(reduction='sum')
+        elif self.loss_type == 'SL':
+            criterion = nn.MSELoss(reduction='sum')
+        else:
+            raise ValueError('Invalid loss type')
+
+        optimizer = optim.Adam(self.parameters(), lr=self.lr)
+
+        last_loss = 0.
+        for epoch in range(1, self.epochs + 1):
+            self.train()
+
+            current_loss = 0.
+            pbar = tqdm(train_loader)
+            pbar.set_description(f'[Epoch {epoch:03d}]')
+            for _, ur, mask_ur in pbar:
+                if torch.cuda.is_available():
+                    ur = ur.cuda()
+                    mask_ur = mask_ur.cuda()
+                else:
+                    ur = ur.cpu()
+                    mask_ur = mask_ur.cpu()
+
+                self.zero_grad()
+                ur = ur.float()
+                mask_ur = mask_ur.float()
+
+                pred, mu, logvar = self.forward(mask_ur)
+                # BCE
+                # BCE = -torch.mean(torch.sum(F.log_softmax(pred, 1) * ur, -1))
+                loss = criterion(pred * mask_ur, ur * mask_ur)
+                KLD = -0.5 * torch.mean(torch.sum(1 + logvar - mu.pow(2) - logvar.exp(), dim=1))
+                loss += KLD
+
+                for layer in self.q_layers:
+                    loss += self.reg_1 * layer.weight.norm(p=1)
+                    loss += self.reg_2 * layer.weight.norm()
+                for layer in self.p_layers:
+                    loss += self.reg_1 * layer.weight.norm(p=1)
+                    loss += self.reg_2 * layer.weight.norm()
+
+                if torch.isnan(loss):
+                    raise ValueError(f'Loss=Nan or Infinity: current settings does not fit the recommender')
+                
+                loss.backward()
+                optimizer.step()
+                pbar.set_postfix(loss=loss.item())
+
+            self.eval()
+            delta_loss = float(current_loss - last_loss)
+            if (abs(delta_loss) < 1e-5) and self.early_stop:
+                print('Satisfy early stop mechanism')
+                break
+            else:
+                last_loss = current_loss
+
+        # TODO pre-predict
+        x_items = torch.tensor(self.rating_mat).float()
+        self.prediction = self.forward(x_items)
+        self.prediction.clamp_(min=0, max=5)
+        self.prediction = self.prediction.detach().numpy()
+        
+
+    def predict(self, u, i):
+        return self.prediction[u, i]
