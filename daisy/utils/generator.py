20a3c84f2075b17b163a43f007c41da7712060d0
yudi <yudi@shanshu.ai>
2020-03-26 15:26:47 +0800

    shrink code

---
+++ daisy/utils/generator.py
@@ -0,0 +1,29 @@
+import os
+import gc
+from daisy.utils.loader import load_rate, split_test
+
+
+
+dataset_list = ['netflix', 'ml-100k', 'ml-1m', 'ml-10m', 'ml-20m', 'lastfm', 'bx', 'amazon-cloth',
+                'amazon-electronic', 'amazon-book', 'amazon-music', 'epinions', 'yelp', 'citeulike']
+
+prepro_list = ['5core', '10core'] # 'origin', 
+
+if not os.path.exists('./experiment_data/'):
+    os.makedirs('./experiment_data/')
+
+for dataset in dataset_list:
+    print(dataset)
+    for prepro in prepro_list:
+        print(prepro)
+        df, user_num, item_num = load_rate(dataset, prepro, False)
+        for test_method in ['fo', 'loo', 'tloo', 'tfo']:
+            print(test_method)
+            train_set, test_set = split_test(df, test_method, .2)
+            train_set.to_csv(f'./experiment_data/train_{dataset}_{prepro}_{test_method}.dat', index=False)
+            test_set.to_csv(f'./experiment_data/test_{dataset}_{prepro}_{test_method}.dat', index=False)
+            
+            print('Finish save train and test set......')
+
+        del train_set, test_set, df
+        gc.collect()
--- main.py
+++ main.py
@@ -10,125 +10,13 @@ import torch
 import torch.utils.data as data
 
 from daisy.utils.loader import *
-from daisy.utils.metrics import *
+from daisy.utils.parser import parse_args
+from daisy.utils.metrics import precision_at_k, recall_at_k, map_at_k, hr_at_k, ndcg_at_k, mrr_at_k
+
 
 if __name__ == '__main__':
     ''' all parameter part '''
-    parser = argparse.ArgumentParser(description='test recommender')
-    # common settings
-    parser.add_argument('--problem_type', 
-                        type=str, 
-                        default='point', 
-                        help='pair-wise or point-wise')
-    parser.add_argument('--algo_name', 
-                        type=str, 
-                        default='vae', 
-                        help='algorithm to choose')
-    parser.add_argument('--dataset', 
-                        type=str, 
-                        default='ml-100k', 
-                        help='select dataset')
-    parser.add_argument('--prepro', 
-                        type=str, 
-                        default='10core', 
-                        help='dataset preprocess op.: origin/Ncore')
-    parser.add_argument('--topk', 
-                        type=int, 
-                        default=50, 
-                        help='top number of recommend list')
-    parser.add_argument('--test_method', 
-                        type=str, 
-                        default='tfo', 
-                        help='method for split test,options: ufo/loo/fo/tfo/tloo')
-    parser.add_argument('--val_method', 
-                        type=str, 
-                        default='tfo', 
-                        help='validation method, options: cv, tfo, loo, tloo')
-    parser.add_argument('--test_size', 
-                        type=float, 
-                        default=.2, 
-                        help='split ratio for test set')
-    parser.add_argument('--val_size', 
-                        type=float, 
-                        default=.1, help='split ratio for validation set')
-    parser.add_argument('--fold_num', 
-                        type=int, 
-                        default=5, 
-                        help='No. of folds for cross-validation')
-    parser.add_argument('--cand_num', 
-                        type=int, 
-                        default=1000, 
-                        help='No. of candidates item for predict')
-    parser.add_argument('--sample_method', 
-                        type=str, 
-                        default='uniform', 
-                        help='negative sampling method mixed with uniform, options: item-ascd, item-desc')
-    parser.add_argument('--sample_ratio', 
-                        type=float, 
-                        default=0, 
-                        help='mix sample method ratio, 0 for all uniform')
-    parser.add_argument('--init_method', 
-                        type=str, 
-                        default='', 
-                        help='weight initialization method')
-    parser.add_argument('--gpu', 
-                        type=str, 
-                        default='0', 
-                        help='gpu card ID')
-    parser.add_argument('--num_ng', 
-                        type=int, 
-                        default=0, 
-                        help='negative sampling number')
-    parser.add_argument('--loss_type', 
-                        type=str, 
-                        default='CL', 
-                        help='loss function type')
-    # algo settings
-    parser.add_argument('--factors', 
-                        type=int, 
-                        default=32, 
-                        help='latent factors numbers in the model')
-    parser.add_argument('--reg_1', 
-                        type=float, 
-                        default=0.001, 
-                        help='L1 regularization')
-    parser.add_argument('--reg_2', 
-                        type=float, 
-                        default=0.001, 
-                        help='L2 regularization')
-    parser.add_argument('--dropout', 
-                        type=float, 
-                        default=0.5, 
-                        help='dropout rate')
-    parser.add_argument('--lr', 
-                        type=float, 
-                        default=0.001, 
-                        help='learning rate')
-    parser.add_argument('--epochs', 
-                        type=int, 
-                        default=20, 
-                        help='training epochs')
-    parser.add_argument('--batch_size', 
-                        type=int, 
-                        default=128, 
-                        help='batch size for training')
-    parser.add_argument('--num_layers', 
-                        type=int, 
-                        default=2, 
-                        help='number of layers in MLP model')
-    parser.add_argument('--act_func', 
-                        type=str, 
-                        default='relu', 
-                        help='activation method in interio layers')
-    parser.add_argument('--out_func', 
-                        type=str, 
-                        default='sigmoid', 
-                        help='activation method in output layers')
-    parser.add_argument('--no_batch_norm', 
-                        action='store_false', 
-                        default=True, 
-                        help='whether do batch normalization in interior layers')
-    args = parser.parse_args()
+    args = parse_args()
 
     # store running time in time_log file
     time_log = open('time_log.txt', 'a') 
