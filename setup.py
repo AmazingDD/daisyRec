1d9f608eb4862a1b4df1244b231f3da084533c3e
amazingDD <1172392977@gmail.com>
2022-07-20 17:33:17 +0800

    update

--- setup.py
+++ setup.py
@@ -3,11 +3,13 @@ from setuptools import setup, find_packages
 
 classifiers = ["License :: OSI Approved :: MIT License"]
 
+
+long_description = 'daisyRec is a Python toolkit developed for benchmarking top-N recommendation task.' \
+                   'The name DAISY stands for multi-Dimension fAirly comparIson for recommender SYstem.'
+
 here = path.abspath(path.dirname(__file__))
-
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
+# with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+#     long_description = f.read()
 with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
     all_reqs = f.read().split('\n')
 
@@ -23,8 +25,9 @@ setup(
     name = 'daisyRec',
     packages = [package for package in find_packages() if package.startswith('daisy')],
     version = 'v2.0.0',  # Ideally should be same as your GitHub release tag varsion
+    description=('An easy-to-use library for recommender systems.'),
     long_description = long_description,
-    long_description_content_type="text/markdown",
+    # long_description_content_type="text/markdown",
     author = 'Yu Di',
     author_email = 'di.yu.2021@mitb.smu.edu.sg',
     url = 'https://github.com/AmazingDD/daisyRec',
