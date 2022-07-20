c0436118e26a4bd8f8ba77d563b2000301da9e11
amazingDD <1172392977@gmail.com>
2022-07-20 19:24:55 +0800

    update 2.0.3 test

--- setup.py
+++ setup.py
@@ -7,13 +7,25 @@ classifiers = ["License :: OSI Approved :: MIT License"]
 long_description = 'daisyRec is a Python toolkit developed for benchmarking top-N recommendation task.' \
                    'The name DAISY stands for multi-Dimension fAirly comparIson for recommender SYstem.'
 
-here = path.abspath(path.dirname(__file__))
+# here = path.abspath(path.dirname(__file__))
 # with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 #     long_description = f.read()
-with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
-    all_reqs = f.read().split('\n')
+# with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+#     all_reqs = f.read().split('\n')
 
-install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
+# install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
+
+install_requires = [
+    'torch>=1.1.0',
+    'numpy>=1.18.0',
+    'scikit-learn>=1.0.2',
+    'scipy>=1.5.2',
+    'pandas>=1.0.5',
+    'tqdm>=4.48.2',
+    'pyyaml>=5.1.0',
+    'colorlog==4.7.2',
+    'colorama==0.4.4',
+]
 
 setup_requires = []
 
@@ -25,14 +37,14 @@ setup(
     name = 'daisyRec',
     # packages = [package for package in find_packages() if package.startswith('daisy')],
     packages = find_packages(exclude=['tests*']),
-    version = 'v2.0.2',  # Ideally should be same as your GitHub release tag varsion
+    version = 'v2.0.3',  # Ideally should be same as your GitHub release tag varsion
     description=('An easy-to-use library for recommender systems.'),
     long_description = long_description,
     # long_description_content_type="text/markdown",
     author = 'Yu Di',
     author_email = 'di.yu.2021@mitb.smu.edu.sg',
     url = 'https://github.com/AmazingDD/daisyRec',
-    download_url = 'https://github.com/AmazingDD/daisyRec/archive/refs/tags/v2.0.2.tar.gz',
+    download_url = 'https://github.com/AmazingDD/daisyRec/archive/refs/tags/v2.0.3.tar.gz',
     keywords = ['ranking', 'recommendation'],
     include_package_data=True,
     install_requires=install_requires,
